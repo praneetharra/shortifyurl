c202b8b13ea86488cd4e0b53e0cb842d077f7134
Rakesh Vijayakumar <rakeshvit10@gmail.com>
2020-01-28 20:41:58 -0600

    Add logging

--- src/main/java/com/shortifyurl/shortify/controller/ShortifyUrlController.java
+++ src/main/java/com/shortifyurl/shortify/controller/ShortifyUrlController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,20 +20,29 @@ import com.shortifyurl.shortify.service.UrlMappingService;
 @RestController
 public class ShortifyUrlController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ShortifyUrlController.class);
+
 	@Autowired
 	UrlMappingService urlMappingService;
 
 	@PostMapping("/create")
 	@ResponseBody
-	public UrlMapping shortifyUrl(@RequestBody String longUrl) {
+	public UrlMapping createShortUrl(@RequestBody String longUrl) {
+		LOG.info("Request recieved for createShortUrl with param: " + longUrl);
 		// handle edge cases
 		return urlMappingService.saveUrlMapping(longUrl);
 	}
 
 	@GetMapping("/{shortUrl}")
 	public void getLongUrl(@PathVariable String shortUrl, HttpServletResponse httpServletResponse) throws IOException {
+		LOG.info("Request recieved for getLongUrl for short url: " + shortUrl);
+
+		String longUrl = urlMappingService.getLongUrlByShortUrl(shortUrl);
 		// it doesn't redirect if url doesn't contain http://www. as prefix
-		httpServletResponse.setHeader("Location", urlMappingService.getLongUrlByShortUrl(shortUrl));
+		if (longUrl == null) {
+			LOG.debug("Long URL cannot be found for short URL: " + shortUrl);
+		}
+		httpServletResponse.setHeader("Location", longUrl);
 		httpServletResponse.setStatus(302);
 	}
 
